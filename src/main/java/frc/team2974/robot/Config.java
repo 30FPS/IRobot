98885199d18960b27199016431daf8960b976f45
30FPS <lukekchenkk@gmail.com>
2018-12-30 18:22:59 -0500

    a

--- src/main/java/frc/team2974/robot/Config.java
+++ src/main/java/frc/team2974/robot/Config.java
@@ -1,4 +1,14 @@
 package frc.team2974.robot;
 
 public class Config {
+
+    public class Input {
+
+        public class Joystick {
+            public static final int LEFT_JOYSTICK_PORT = 0;
+            public static final int RIGHT_JOYSTICK_PORT =1;
+        }
+    }
+
+
 }
