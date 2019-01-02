93c7fa9e24af6fd9cf49a2120a6ef159d4cd0b57
30FPS <lukekchenkk@gmail.com>
2019-01-01 22:53:07 -0500

    1/1/19

--- src/main/java/frc/team2974/robot/Config.java
+++ src/main/java/frc/team2974/robot/Config.java
@@ -10,5 +10,15 @@ public class Config {
         }
     }
 
+    public class Motor {
+        public static final int LEFT_MOTOR_PORT = 0;
+        public static final int RIGHT_MOTOR_PORT = 1;
+        public static final int SOLENOID_PORT = 2;
+    }
 
+    public class Encoder {
+        public static final int LEFT_ENCODER_CHANNEL = 0;
+        public static final int RIGHT_ENCODER_CHANNEL = 1;
+        public static final double ENCODER_DISTANCE_PER_PULSE = 0.000001;
+    }
 }
