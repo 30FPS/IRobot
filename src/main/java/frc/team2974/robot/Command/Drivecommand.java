0167b38ba87907da9760a46475d596344e0435e6
30FPS <lukekchenkk@gmail.com>
2019-01-02 17:48:19 -0500

    1/2/19 (template finished, yet to innovate)

--- src/main/java/frc/team2974/robot/Command/Drivecommand.java
+++ src/main/java/frc/team2974/robot/Command/Drivecommand.java
@@ -1,4 +1,45 @@
 package frc.team2974.robot.Command;
 
-public class Drivecommand {
+import edu.wpi.first.wpilibj.command.Command;
+
+import static frc.team2974.robot.Config.Input.Deadzone.CONTROLLER_DEADZONE;
+import static frc.team2974.robot.OI.*;
+import static frc.team2974.robot.Robot.drivetrain;
+
+public class DriveCommand extends Command {
+
+    public DriveCommand() {
+        requires(drivetrain);
+    }
+
+    @Override
+    protected boolean isFinished() {
+        return false;
+    }
+
+    @Override
+    protected void execute() {
+        double leftPos = leftJoystick.getY();
+        double rightPos = rightJoystick.getY();
+
+        if (Math.abs(leftPos) < CONTROLLER_DEADZONE) {
+            leftPos = 0;
+        }
+        if (Math.abs(rightPos) < CONTROLLER_DEADZONE) {
+            rightPos = 0;
+        }
+
+        drivetrain.setSpeeds(leftPos, rightPos);
+
+        if(shiftUp.get()) {
+            drivetrain.shiftUp();
+        } else if (shiftDown.get()) {
+            drivetrain.shiftDown();
+        }
+    }
+
+    @Override
+    protected void end() {
+        super.end();
+    }
 }
